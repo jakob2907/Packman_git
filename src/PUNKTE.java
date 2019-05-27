176f164eb30e123ef358ea2c95d4046b76868dc0
Holfelder.Marie <ukzzf>
2019-05-27 08:39:41 +0200

    1

--- src/PUNKTE.java
+++ src/PUNKTE.java
@@ -2,14 +2,18 @@ public class PUNKTE
 {
     PUNKTE [] punkte;
 
-    int YKo;
-    int XKo;
 
-    public PUNKTE (int YKoNew,int XKoNew )
+
+    public PUNKTE (int XKoNew,int YKoNew )
     {
         punkte = new PUNKTE [VAR.Punkteanzahl];
 
-                punkte[0] = new PUNKT(188,123);
+        punkte[0] = new PUNKTE(188,123);
+        punkte[1] = new PUNKTE(203,123);
+        punkte[2] = new PUNKTE(218,123);
+        punkte[3] = new PUNKTE(233,123);
+        punkte[4] = new PUNKTE(248,123);
+        punkte[5] = new PUNKTE(263,123);
 
     }
 
